7834fe49008300a66b3364f0fd077cdac9969acd
viniciusgomes2005 <vinixviii@gmail.com>
2025-03-11 14:32:15 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -1,15 +1,23 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include <math.h>
 #include <stdio.h>
+#include <time.h>
 
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+uint64_t future_time = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
-        flag_f_r = 1;
+        if(flag_f_r){
+
+            flag_f_r = 0;
+        }else{
+            flag_f_r = 1;
+        }
     } else if (events == 0x8) { // rise edge
     }
 }
@@ -25,11 +33,17 @@ int main() {
 
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
-
+    future_time = time_us_64() + 500000;
     while (true) {
+        uint64_t now = time_us_64(); 
+        if (now >= future_time) {
+            future_time = now + 500000; 
 
-        if (flag_f_r) {
-            flag_f_r = 0;
+            if (flag_f_r) {
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+            }
         }
+
+        sleep_ms(1);
     }
 }
